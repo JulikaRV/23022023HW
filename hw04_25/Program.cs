2b150b69459f2005cbbb62bf8fac9bfe5d4d098e
Julia <julia.rogovaja@gmail.com>
2023-03-24 12:01:11 +0200

    .

---
+++ hw04_25/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+var rnd = new Random();
+
+int num1 = rnd.Next(2, 10);
+System.Console.WriteLine($" Число A => {num1}");
+int num2 = rnd.Next(1, 10);
+System.Console.WriteLine($" Число B => {num2}");
+
+// 1 способ
+
+int Pow(int num, int n)
+{
+    int powNum = 1;
+    for (int i = 0; i < n; i++)
+    {
+        powNum *= num;
+    }
+    return powNum;
+}
+
+// 2 способ
+
+int Pow2(int num, int n)
+{
+    int powNum = 1;
+    while (n != 0)
+    {
+        powNum *= num;
+        n = n - 1;
+    }
+    return powNum;
+}
+
+int result = Pow(num1, num2);
+System.Console.WriteLine($"Число A в натуральной степени B = {result}");
+
+int result2 = Pow2(num1, num2);
+System.Console.WriteLine($"Число A в натуральной степени B = {result2}");
---
+++ hw04_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, 
+// заполненный псевдослучайными числами и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+    return arr;
+}
+
+int[] newArray = GetArray(8);
+System.Console.WriteLine();
+System.Console.WriteLine($"[{String.Join(',', newArray)}]");
+
