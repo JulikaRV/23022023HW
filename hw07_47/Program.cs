76776e9828a9318ebb44f55845c857f082b36a51
Julia <julia.rogovaja@gmail.com>
2023-04-10 18:48:11 +0300

    .

--- hw07_47/Program.cs
+++ hw07_47/Program.cs
@@ -10,9 +10,9 @@
 double[,] a = new double[5, 10];
 
 Random random = new Random();
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < a.GetLength(0); i++)
 {
-    for (int j = 0; j < 10; j++)
+    for (int j = 0; j < a.GetLength(1); j++)
     {
         a[i, j] = random.NextDouble() * 100;
         Console.Write("{0,6:F2}", a[i, j]);
